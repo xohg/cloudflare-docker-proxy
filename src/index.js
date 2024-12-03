83293af07493fc4a0b3d37971c50bf8eeaeab26d
xohg <54381330+xohg@users.noreply.github.com>
2024-12-03 14:52:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -161,3 +161,15 @@ function responseUnauthorized(url) {
     headers: headers,
   });
 }
+
+import DOCS from './help.html'
+ 
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
